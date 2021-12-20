45f65e1a7b7d74987fb0ab617704f102a0736f4a
Pierre Tardy <tardyp@gmail.com>
2021-12-20 16:54:34 +0100

    fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,29 +3,24 @@
 import os
 import sys
 from pkgutil import walk_packages
+from sphinx.testing.fixtures import make_app, test_params
+from sphinx.testing.util import path as Path
 
-from sphinx_testing import with_app
+if sys.version_info[:2] > (3, 8):
+    import collections
+    import collections.abc
+    collections.Mapping = collections.abc.Mapping
+    collections.MutableMapping = collections.abc.MutableMapping
 
-if sys.version_info[:2] > (3, 0):
-    # Make sure that the other sphinxcontrib packages can be loaded
-    import sphinxcontrib.jinja
-    if 'site-packages' not in sphinxcontrib.__path__:
-        sys.path.remove(os.path.dirname(sphinxcontrib.__path__[0]))
-        del sys.modules['sphinxcontrib']
-        for path in filter(lambda p: 'site-packages' in p, sys.path):
-            contrib = os.path.join(path, 'sphinxcontrib')
-            for minfo in walk_packages([contrib], prefix='sphinxcontrib.'):
-                spec = minfo.module_finder.find_spec(minfo.name)
-                spec.loader.load_module(minfo.name)
+SRCDIR = Path(__file__).parent / "docs/basic"
 
-
-@with_app(buildername='html', srcdir='tests/docs/basic/')
-def test_build_html(app, status, warning):
+def test_build_html(make_app):
+    app = make_app(buildername='html', srcdir=SRCDIR)
     app.builder.build_all()
 
 
-@with_app(buildername='singlehtml', srcdir='tests/docs/basic/')
-def test_build_singlehtml(app, status, warning):
+def test_build_singlehtml(make_app):
+    app = make_app(buildername='singlehtml', srcdir=SRCDIR)
     app.builder.build_all()
     html = (app.outdir / 'index.html').read_text()
     assert ('<p>A sphinx extension to include jinja based templates based '
@@ -34,23 +29,23 @@ def test_build_singlehtml(app, status, warning):
     assert '<p>second:a = b</p>' in html
 
 
-@with_app(buildername='latex', srcdir='tests/docs/basic/')
-def test_build_latex(app, status, warning):
+def test_build_latex(make_app):
+    app = make_app(buildername='latex', srcdir=SRCDIR)
     app.builder.build_all()
 
 
-@with_app(buildername='epub', srcdir='tests/docs/basic/')
-def test_build_epub(app, status, warning):
+def test_build_epub(make_app):
+    app = make_app(buildername='epub', srcdir=SRCDIR)
     app.builder.build_all()
 
 
-@with_app(buildername='json', srcdir='tests/docs/basic/')
-def test_build_json(app, status, warning):
+def test_build_json(make_app):
+    app = make_app(buildername='json', srcdir=SRCDIR)
     app.builder.build_all()
 
 
-@with_app(buildername='singlehtml', srcdir='tests/docs/basic/')
-def test_customize_env(app, status, warning):
+def test_customize_env(make_app):
+    app = make_app(buildername='singlehtml', srcdir=SRCDIR)
     app.builder.build_all()
     html = (app.outdir / 'index.html').read_text()
     assert '<h2>Lists' in html
